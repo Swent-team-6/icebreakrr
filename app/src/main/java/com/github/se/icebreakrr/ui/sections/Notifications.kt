8f585f831bd7cfdab931ac3469eec2d506162eb0
arthur <arthur@margeat.com>
2024-10-10 20:46:25 +0200

    feat(ui): add NotificationScreen with bottom navigation bar
    - Created `NotificationScreen` composable displaying main content and bottom nav bar
    - Integrated `BottomNavigationMenu` for navigating between top-level destinations
    - Connected navigation actions to handle tab selection and screen transitions
    - Added a test tag for UI testing on the notification screen

---
+++ app/src/main/java/com/github/se/icebreakrr/ui/sections/Notifications.kt
@@ -0,0 +1,32 @@
+package com.github.se.icebreakrr.ui.sections
+
+import android.annotation.SuppressLint
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import com.github.se.icebreakrr.ui.navigation.BottomNavigationMenu
+import com.github.se.icebreakrr.ui.navigation.LIST_TOP_LEVEL_DESTINATIONS
+import com.github.se.icebreakrr.ui.navigation.NavigationActions
+
+/**
+ * Composable function for displaying the notification screen.
+ *
+ * It includes a bottom navigation bar and displays the main content of the notification screen.
+ *
+ * @param navigationActions The actions used for navigating between screens.
+ */
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun NotificationScreen(navigationActions: NavigationActions) {
+  Scaffold(
+      modifier = Modifier.testTag("aroundYouScreen"),
+      bottomBar = {
+        BottomNavigationMenu(
+            onTabSelect = { route -> navigationActions.navigateTo(route) },
+            tabList = LIST_TOP_LEVEL_DESTINATIONS,
+            selectedItem = navigationActions.currentRoute())
+      },
+      content = { Text(text = "Notification screen") })
+}
