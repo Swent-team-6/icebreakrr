5a23b95e454c0c87d211d1a3df2afd4ed588f52a
JanStaszewiczEPFL <jan.staszewicz@epfl.ch>
2024-10-13 18:16:28 +0200

    fix(tests): remove conflicting packages and code from SignInScreen.kt test

--- app/src/androidTest/java/com/github/se/icebreakrr/ui/authentication/SignInScreen.kt
+++ app/src/androidTest/java/com/github/se/icebreakrr/ui/authentication/SignInScreen.kt
@@ -9,9 +9,6 @@ import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
-import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
-import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.se.icebreakrr.MainActivity
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
@@ -23,11 +20,6 @@ import org.junit.runner.RunWith
 class LoginTest : TestCase() {
   @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
 
-  // The IntentsTestRule simply calls Intents.init() before the @Test block
-  // and Intents.release() after the @Test block is completed. IntentsTestRule
-  // is deprecated, but it was MUCH faster than using IntentsRule in our tests
-  @get:Rule val intentsTestRule = IntentsTestRule(MainActivity::class.java)
-
   @Test
   fun titleAndButtonAreCorrectlyDisplayed() {
     composeTestRule.onNodeWithTag("loginTitle").assertIsDisplayed()
@@ -35,13 +27,5 @@ class LoginTest : TestCase() {
 
     composeTestRule.onNodeWithTag("loginButton").assertIsDisplayed()
     composeTestRule.onNodeWithTag("loginButton").assertHasClickAction()
-  }
-
-  @Test
-  fun googleSignInReturnsValidActivityResult() {
-    composeTestRule.onNodeWithTag("loginButton").performClick()
-
-    // assert that an Intent resolving to Google Mobile Services has been sent (for sign-in)
-    intended(toPackage("com.google.android.gms"))
   }
 }
