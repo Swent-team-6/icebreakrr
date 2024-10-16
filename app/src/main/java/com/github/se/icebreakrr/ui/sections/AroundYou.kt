218703ec3be59dd04512a8e0f8bde74dd6a56d6b
Valentin Porchet <val.porchet@gmail.com>
2024-10-16 17:33:50 +0200

    feat(overview): add FAB and change signature

--- app/src/main/java/com/github/se/icebreakrr/ui/sections/AroundYou.kt
+++ app/src/main/java/com/github/se/icebreakrr/ui/sections/AroundYou.kt
@@ -10,21 +10,32 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.outlined.Warning
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.github.se.icebreakrr.R
 import com.github.se.icebreakrr.ui.Profile
+import com.github.se.icebreakrr.ui.ProfileViewModel
 import com.github.se.icebreakrr.ui.navigation.BottomNavigationMenu
 import com.github.se.icebreakrr.ui.navigation.LIST_TOP_LEVEL_DESTINATIONS
 import com.github.se.icebreakrr.ui.navigation.NavigationActions
+import com.github.se.icebreakrr.ui.navigation.Screen
 import com.github.se.icebreakrr.ui.navigation.shared.ProfileCard
 
 
@@ -37,71 +48,82 @@ import com.github.se.icebreakrr.ui.navigation.shared.ProfileCard
  */
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun AroundYouScreen(navigationActions: NavigationActions) {
-    //todo: when viewmodel is implemented, replace this with viewmodel's data
-    val twoProfiles = Profile.getMockedProfiles()
-    val empty = emptyList<Profile>()
+fun AroundYouScreen(
+    navigationActions: NavigationActions,
+    profileViewModel: ProfileViewModel = viewModel(factory = ProfileViewModel.Factory)
+) {
 
-    val profiles = twoProfiles //todo: remove me after debugging
+    val profiles = profileViewModel.profiles.collectAsState()
 
 
-  Scaffold(
-      modifier = Modifier.testTag("aroundYouScreen"),
-      bottomBar = {
-        BottomNavigationMenu(
-            onTabSelect = { route -> navigationActions.navigateTo(route) },
-            tabList = LIST_TOP_LEVEL_DESTINATIONS,
-            selectedItem = navigationActions.currentRoute())
-      },
+    Scaffold(
+        modifier = Modifier.testTag("aroundYouScreen"),
+        bottomBar = {
+            BottomNavigationMenu(
+                onTabSelect = { route -> navigationActions.navigateTo(route) },
+                tabList = LIST_TOP_LEVEL_DESTINATIONS,
+                selectedItem = navigationActions.currentRoute()
+            )
+        },
 
-      topBar = {
-          Box(
-              contentAlignment = Alignment.Center,
-              modifier = Modifier
-                  .background(Color(0xFF1FAEF0)) //fixme: use icebreakrr blue
-                  .fillMaxWidth()
-                  .heightIn(90.dp)
-          ) {
-              Text(
-                  text = "Around You",
-                  fontSize = 40.sp,
-                  color = Color.White,
-                  fontWeight = FontWeight.Bold,
-                  textAlign = TextAlign.Center,
-                  modifier = Modifier.fillMaxWidth()
+        topBar = {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .background(Color(0xFF1FAEF0)) //fixme: use icebreakrr blue
+                    .fillMaxWidth()
+                    .heightIn(90.dp)
+            ) {
+                Text(
+                    text = "Around You",
+                    fontSize = 40.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
 
-              )
-          }
-      },
-      content = { innerPadding ->
-          if (profiles.isNotEmpty()) {
-              LazyColumn(
-                  contentPadding = PaddingValues(vertical = 16.dp),
-                  verticalArrangement = Arrangement.spacedBy(16.dp),
-                  modifier = Modifier
-                      .fillMaxWidth()
-                      .padding(horizontal = 8.dp)
-                      .padding(innerPadding)){
-                  items(profiles.size) { index ->
-                      ProfileCard(
-                          profile = profiles[index],
-                          onclick = { /* todo: change selected profile in VM and navigate to viewprofile*/ }
-                      )
-                  }
-              }
-          } else {
-              Box (
+                )
+            }
+        },
+        content = { innerPadding ->
+            if (profiles.value.isNotEmpty()) {
+                LazyColumn(
+                    contentPadding = PaddingValues(vertical = 16.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 8.dp)
+                        .padding(innerPadding)
+                ) {
+                    items(profiles.value.size) { index ->
+                        ProfileCard(
+                            profile = profiles.value[index],
+                            onclick = { /* todo: change selected profile in VM and navigate to viewprofile*/ }
+                        )
+                    }
+                }
+            } else {
+                Box(
                     contentAlignment = Alignment.Center,
                     modifier = Modifier.fillMaxSize().padding(innerPadding)
                 ) {
-                  Text(
-                      modifier = Modifier.padding(innerPadding),
-                      text = "There is no one around. Try moving!",
-                      fontSize = 20.sp,
-                      fontWeight = FontWeight.Bold,
-                      color = Color(0xFF575757)
-                  )
-              }
-          }
-      })
+                    Text(
+                        modifier = Modifier.padding(innerPadding),
+                        text = "There is no one around. Try moving!",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF575757)
+                    )
+                }
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /* todo: navigate to add profile */ },
+                containerColor = Color(0xFF1FAEF0) //fixme: use icebreakrr blue
+            ) {
+                Icon(painter = painterResource(R.drawable.filter_24), contentDescription = "Add")
+            }
+        }
+    )
 }
