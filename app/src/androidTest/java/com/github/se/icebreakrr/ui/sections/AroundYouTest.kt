aa250a29ef16e42386c069eff2ff16eec1c84a6e
Valentin Porchet <46000121+Kapestian@users.noreply.github.com>
2024-10-17 20:14:42 +0200

    test(overview): test previously added navigation

--- app/src/androidTest/java/com/github/se/icebreakrr/ui/sections/AroundYouTest.kt
+++ app/src/androidTest/java/com/github/se/icebreakrr/ui/sections/AroundYouTest.kt
@@ -5,14 +5,17 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import com.github.se.icebreakrr.model.profile.MockProfileViewModel
 import com.github.se.icebreakrr.ui.navigation.NavigationActions
 import com.github.se.icebreakrr.ui.navigation.Route
+import com.github.se.icebreakrr.ui.navigation.Screen
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
+import org.mockito.kotlin.verify
 
 class AroundYouTest {
   private lateinit var navigationActions: NavigationActions
@@ -34,6 +37,22 @@ class AroundYouTest {
     mockProfileViewModel.clearProfiles()
     composeTestRule.onNodeWithTag("emptyProfilePrompt").assertIsDisplayed()
   }
+
+  @Test
+  fun navigationOnCardClick() {
+    composeTestRule.onAllNodesWithTag("profileCard").onFirst().assertIsDisplayed()
+    composeTestRule.onAllNodesWithTag("profileCard").onFirst().performClick()
+    verify(navigationActions).navigateTo(screen = Screen.PROFILE_DISPLAY)
+  }
+
+  @Test
+  fun navigationOnFabClick() {
+    composeTestRule.onAllNodesWithTag("filterButton").onFirst().assertIsDisplayed()
+    composeTestRule.onAllNodesWithTag("filterButton").onFirst().performClick()
+    verify(navigationActions).navigateTo(screen = Screen.FILTER)
+  }
+
+
 
   @Test
   fun hasRequiredComponents() {
