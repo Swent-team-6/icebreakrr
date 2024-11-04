3f12eb9974f6413a3e0b7885af20f5379fd97a46
MaxCastelCast <maximo.castelari@gmail.com>
2024-11-05 00:22:56 +0100

    feature : Now the sending of messages work directly with Firebase, and set up of Firebase Cloud Functions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -153,6 +153,7 @@ dependencies {
     implementation(libs.coil.compose)
 
     implementation(libs.firebase.firestore.ktx)
+    implementation(libs.firebase.functions.ktx)
     testImplementation(libs.junit)
     globalTestImplementation(libs.androidx.junit)
     globalTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/github/se/icebreakrr/model/message/MeetingRequestViewModel.kt
+++ app/src/main/java/com/github/se/icebreakrr/model/message/MeetingRequestViewModel.kt
@@ -9,14 +9,10 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.github.se.icebreakrr.model.profile.ProfilesViewModel
 import com.google.firebase.Firebase
+import com.google.firebase.functions.FirebaseFunctions
 import com.google.firebase.messaging.messaging
-import java.io.IOException
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
-import retrofit2.HttpException
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.create
 
 class MeetingRequestViewModel(
     private val profilesViewModel: ProfilesViewModel,
@@ -25,6 +21,7 @@ class MeetingRequestViewModel(
 
   var meetingRequestState by mutableStateOf(MeetingRequest())
   var meetingResponseState by mutableStateOf(MeetingResponse())
+  private val functions = FirebaseFunctions.getInstance()
 
   companion object {
     class Factory(
@@ -42,35 +39,19 @@ class MeetingRequestViewModel(
     }
   }
 
-  private val api: FcmApi =
-      Retrofit.Builder()
-          .baseUrl(
-              "http://10.0.2.2:8080/") // Todo: use an other URL later, using dependency injection ?
-          .addConverterFactory(MoshiConverterFactory.create())
-          .build()
-          .create()
-
   init {
-    viewModelScope.launch { Firebase.messaging.subscribeToTopic("allUsers").await() }
+    viewModelScope.launch {
+      // Subscribe to a topic, or retrieve FCM token if needed
+      Firebase.messaging.subscribeToTopic("allUsers").await()
+    }
   }
 
   fun onRemoteTokenChange(newToken: String) {
-    //    if (ourUserId != null) {
-    //      profilesViewModel.getProfileByUid(ourUserId)
-    //      val currentProfile = profilesViewModel.selectedProfile.value
-    //      if (currentProfile != null) {
-    //        val updatedProfile = currentProfile.copy(fcmToken = newToken)
-    //        profilesViewModel.updateProfile(updatedProfile)
-    //      }
-    //    }
     meetingRequestState = meetingRequestState.copy(targetToken = newToken)
   }
 
   fun onMeetingRequestChange(newMessage: String) {
-    meetingRequestState =
-        meetingRequestState.copy(
-            message = newMessage,
-        )
+    meetingRequestState = meetingRequestState.copy(message = newMessage)
   }
 
   fun onSubmitMeetingRequest() {
@@ -86,32 +67,27 @@ class MeetingRequestViewModel(
     meetingResponseState = meetingResponseState.copy(isEnteringMessage = false)
   }
 
+  // Send a message by calling the Firebase Function to send FCM message
+
   fun sendMessage(isBroadcast: Boolean) {
-    Log.d("SENDING MESSAGE", meetingRequestState.targetToken)
-    //      profilesViewModel.getProfileByUid(ourUserId)
-    //      val userName = profilesViewModel.selectedProfile.value?.name
     viewModelScope.launch {
-      val messageDto =
-          SendMessageDto(
-              to = if (isBroadcast) null else meetingRequestState.targetToken,
-              notification = NotificationBody(title = "Jan", body = meetingRequestState.message))
+      val data =
+          hashMapOf(
+              "targetToken" to meetingRequestState.targetToken,
+              "senderUID" to meetingRequestState.senderUID,
+              "body" to meetingRequestState.message,
+              "picture" to meetingRequestState.picture,
+              "location" to meetingRequestState.location)
       try {
-        if (isBroadcast) {
-          Log.d("MESSAGE", "BROADCAST SENT")
-          api.broadcast(messageDto)
-          Log.d("MESSAGE", "BROADCAST RETURNED")
-        } else {
-          Log.d("MESSAGE", "NOTIFICATION SENT")
-          api.sendMessage(messageDto)
-          Log.d("MESSAGE", "NOTIFICATION RETURNED")
-        }
+        val result =
+            functions
+                .getHttpsCallable("sendMessage") // Cloud Function name
+                .call(data)
+                .await()
+
         meetingRequestState = meetingRequestState.copy(message = "", picture = null)
-      } catch (e: HttpException) {
-        Log.e("API ERROR", "HTTP exception: ${e.code()}", e)
-      } catch (e: IOException) {
-        Log.e("API ERROR", "Network error", e)
       } catch (e: Exception) {
-        Log.e("API ERROR", "Unexpected error", e)
+        Log.e("FIREBASE ERROR", "Error sending message", e)
       }
     }
   }
