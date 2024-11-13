faae0c1dfc6eee7148604bbf6b90890f7f0098e7
arthur <arthur@margeat.com>
2024-11-13 23:30:08 +0100

    feat(main): add location features
    - Initialized `PermissionManager` and set up `LocationViewModel` dependencies
    with `LocationService` and `GeoFirestoreRepository`
    - Added dynamic location updates based on login state with `AuthStateListener`
    - Updated `onResume` to refresh permissions, ensuring they are up-to-date
    - Included `requestNotificationPermission` function as a temporary solution
    for notification permissions, to be replaced by `PermissionManager`

--- app/src/main/java/com/github/se/icebreakrr/MainActivity.kt
+++ app/src/main/java/com/github/se/icebreakrr/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -20,6 +21,9 @@ import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import com.github.se.icebreakrr.config.LocalIsTesting
 import com.github.se.icebreakrr.model.filter.FilterViewModel
+import com.github.se.icebreakrr.model.location.GeoFirestoreRepository
+import com.github.se.icebreakrr.model.location.LocationService
+import com.github.se.icebreakrr.model.location.LocationViewModel
 import com.github.se.icebreakrr.model.message.MeetingRequestManager
 import com.github.se.icebreakrr.model.message.MeetingRequestViewModel
 import com.github.se.icebreakrr.model.profile.ProfilesViewModel
@@ -36,6 +40,9 @@ import com.github.se.icebreakrr.ui.sections.FilterScreen
 import com.github.se.icebreakrr.ui.sections.NotificationScreen
 import com.github.se.icebreakrr.ui.sections.SettingsScreen
 import com.github.se.icebreakrr.ui.theme.SampleAppTheme
+import com.github.se.icebreakrr.utils.PermissionManager
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.functions.FirebaseFunctions
@@ -43,16 +50,51 @@ import com.google.firebase.functions.FirebaseFunctions
 class MainActivity : ComponentActivity() {
   private lateinit var auth: FirebaseAuth
   private lateinit var functions: FirebaseFunctions
+  private lateinit var locationViewModel: LocationViewModel
+  private lateinit var locationService: LocationService
+  private lateinit var geoFirestoreRepository: GeoFirestoreRepository
+  private lateinit var permissionManager: PermissionManager
+  private lateinit var authStateListener: FirebaseAuth.AuthStateListener
+  private lateinit var fusedLocationClient: FusedLocationProviderClient
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
     // Retrieve the testing flag from the Intent
     val isTesting = intent?.getBooleanExtra("IS_TESTING", false) ?: false
 
-    requestNotificationPermission()
+    // Create and initialize the PermissionManager with the list of permissions required
+    requestNotificationPermission() // TODO remove this and use the PermissionManager instead
+    permissionManager = PermissionManager(this)
+    permissionManager.initializeLauncher(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION))
+
+    // Create required dependencies for the LocationViewModel
+    fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    locationService = LocationService(fusedLocationClient)
+    geoFirestoreRepository = GeoFirestoreRepository()
+
+    // Initialize the LocationViewModel with dependencies
+    locationViewModel =
+        ViewModelProvider(
+            this,
+            LocationViewModel.provideFactory(
+                locationService, geoFirestoreRepository, permissionManager))[
+            LocationViewModel::class.java]
+
+    // Initialize Firebase Auth
     FirebaseApp.initializeApp(this)
     auth = FirebaseAuth.getInstance()
     functions = FirebaseFunctions.getInstance()
+
+    // Monitor login/logout events and perform the necessary actions.
+    authStateListener =
+        FirebaseAuth.AuthStateListener { firebaseAuth ->
+          if (firebaseAuth.currentUser != null) {
+            locationViewModel.tryToStartLocationUpdates()
+          } else {
+            locationViewModel.stopLocationUpdates()
+          }
+        }
 
     setContent {
       // Provide the `isTesting` flag to the entire composable tree
@@ -64,6 +106,26 @@ class MainActivity : ComponentActivity() {
     }
   }
 
+  override fun onStart() {
+    super.onStart()
+    // Add the AuthStateListener when the activity starts
+    auth.addAuthStateListener(authStateListener)
+  }
+
+  override fun onResume() {
+    super.onResume()
+    // Update permissions when the activity resumes to ensure they are up-to-date
+    permissionManager.updateAllPermissions()
+  }
+
+  override fun onStop() {
+    super.onStop()
+    // Remove the AuthStateListener when the activity stops
+    auth.removeAuthStateListener(authStateListener)
+  }
+
+  // TODO remove this and use the PermissionManager instead
+  // TODO the permissions must be asked by the class requiring it
   private fun requestNotificationPermission() {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
       val hasPermission =
