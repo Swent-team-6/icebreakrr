c291ba3eb9a6f49cb93170aca74d13a60233d7b0
arthur <arthur@margeat.com>
2024-11-14 23:47:54 +0100

    feat(location): add LocationRepository interface
    - Adds `setUserPosition` function to update the user's geographic location in
    Firestore using a GeoPoint
    - Improves location data management and integration with Firestore for user
    tracking

---
+++ app/src/main/java/com/github/se/icebreakrr/model/location/LocationRepository.kt
@@ -0,0 +1,12 @@
+package com.github.se.icebreakrr.model.location
+
+import com.google.firebase.firestore.GeoPoint
+
+interface LocationRepository {
+  /**
+   * Sets the user's geographic location in Firestore.
+   *
+   * @param newLocation The new geographic location of the user as a `GeoPoint`.
+   */
+  fun setUserPosition(newLocation: GeoPoint)
+}
