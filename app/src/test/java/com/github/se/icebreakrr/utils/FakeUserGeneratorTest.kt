242889de07bbbe364ae223a183d2b5f9f5936c9e
JanStaszewiczEPFL <jan.staszewicz@epfl.ch>
2024-11-29 00:34:12 +0100

    test: add unit tests for FakeUserGenerator.kt functionality

---
+++ app/src/test/java/com/github/se/icebreakrr/utils/FakeUserGeneratorTest.kt
@@ -0,0 +1,51 @@
+package com.github.se.icebreakrr.utils
+
+import com.github.se.icebreakrr.model.profile.ProfilesViewModel
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.GeoPoint
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.*
+import org.mockito.kotlin.argThat
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.whenever
+
+class FakeUserGeneratorTest {
+
+    private lateinit var profilesViewModel: ProfilesViewModel
+
+    @Before
+    fun setUp() {
+        profilesViewModel = mock(ProfilesViewModel::class.java)
+    }
+
+    @Test
+    fun generateFakeUsers_callsAddNewProfile() {
+        // Given
+        val centerLat = 0.0
+        val centerLon = 0.0
+        val radius = 1000.0
+
+        // When
+        generateFakeUsers(centerLat, centerLon, radius, profilesViewModel)
+
+        // Then
+        for (userId in predefinedUserIds) {
+            verify(profilesViewModel).addNewProfile(argThat { 
+                uid == userId && name == "Fake User $userId" 
+            })
+        }
+    }
+
+    @Test
+    fun deleteFakeUsers_callsDeleteProfileByUid() {
+        // When
+        deleteFakeUsers(profilesViewModel)
+
+        // Then
+        for (userId in predefinedUserIds) {
+            verify(profilesViewModel).deleteProfileByUid(eq(userId))
+        }
+    }
+}
+
