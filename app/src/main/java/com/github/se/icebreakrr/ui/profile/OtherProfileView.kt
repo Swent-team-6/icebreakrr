20fe262994d3e096dbeda16c18f7f530e54d4917
Valentin Porchet <46000121+Kapestian@users.noreply.github.com>
2024-12-06 00:31:52 +0100

    feat(AI UI): add sparkle icon for AI button

--- app/src/main/java/com/github/se/icebreakrr/ui/profile/OtherProfileView.kt
+++ app/src/main/java/com/github/se/icebreakrr/ui/profile/OtherProfileView.kt
@@ -16,8 +16,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -40,6 +38,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
@@ -157,9 +156,10 @@ fun OtherProfileView(
                           Spacer(modifier = Modifier.width(ICON_SPACING))
 
                           Icon(
-                              imageVector = Icons.Filled.Warning,
-                              contentDescription = "AI icon") // todo: replace by AI icon
-                    }
+                              painter = painterResource(id = R.drawable.sparkles),
+                              contentDescription = "AI icon",
+                              tint = MaterialTheme.colorScheme.onPrimary)
+                        }
                   }
 
               // Already met button
