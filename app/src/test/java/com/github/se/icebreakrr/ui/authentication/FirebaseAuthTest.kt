25157994a86b71f1b6ba1ad39b10f7541f8465fa
Louis Cresci <louis@cresci.eu>
2024-12-05 18:51:20 +0100

    fix: fix the bugs in tests

--- app/src/test/java/com/github/se/icebreakrr/ui/authentication/FirebaseAuthTest.kt
+++ app/src/test/java/com/github/se/icebreakrr/ui/authentication/FirebaseAuthTest.kt
@@ -17,9 +17,6 @@ import org.mockito.kotlin.*
 class FirebaseAuthTest {
 
   private val testDispatcher = TestCoroutineDispatcher()
-  private val testScope =
-      createTestCoroutineScope(
-          TestCoroutineDispatcher() + TestCoroutineExceptionHandler() + testDispatcher)
 
   private lateinit var mockFirebaseAuth: FirebaseAuth
   private lateinit var mockFirebaseUser: FirebaseUser
