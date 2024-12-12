f9b2386c4bcf71ab42a93ef2dad684620f59e80e
Louis Cresci <louis@cresci.eu>
2024-12-12 15:18:57 +0100

    chore: fix the notification screen options

--- app/src/main/java/com/github/se/icebreakrr/ui/sections/Notifications.kt
+++ app/src/main/java/com/github/se/icebreakrr/ui/sections/Notifications.kt
@@ -113,6 +113,7 @@ fun NotificationScreen(
                   context,
                   navigationActions)
             }
+            MeetingRequestOption.CHOOSE_LOCATION -> {}
           }
         }
       })
