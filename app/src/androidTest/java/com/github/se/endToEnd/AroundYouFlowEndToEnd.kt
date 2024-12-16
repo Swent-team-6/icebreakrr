650cdf70a202fade62d6cd41aef118b7aa41d668
JanStaszewiczEPFL <jan.staszewicz@epfl.ch>
2024-12-16 20:01:51 +0100

    fix(test): uncommenting necessary class

--- app/src/androidTest/java/com/github/se/endToEnd/AroundYouFlowEndToEnd.kt
+++ app/src/androidTest/java/com/github/se/endToEnd/AroundYouFlowEndToEnd.kt
@@ -1,37 +1,12 @@
-/*
 package com.github.se.endToEnd
 
 // import com.google.firebase.functions.dagger.Provides
 
 import android.app.Activity
 import android.content.Intent
-import androidx.compose.ui.test.assertHasClickAction
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertIsNotDisplayed
-import androidx.compose.ui.test.assertIsNotSelected
-import androidx.compose.ui.test.assertIsSelected
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performScrollTo
-import androidx.compose.ui.test.performTextClearance
-import androidx.compose.ui.test.performTextInput
 import androidx.core.app.ActivityCompat
-import androidx.test.core.app.ActivityScenario
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import com.github.se.icebreakrr.MainActivity
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
-import javax.inject.Inject
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
-import org.junit.runner.RunWith
 
+/*
 private const val ALICE = "Alice Inwonderland"
 
 @RunWith(AndroidJUnit4::class)
@@ -196,7 +171,7 @@ class AroundYouFlowEndToEnd {
     }
   }
 }
-
+*/
 class TestPermissionDelegate : ActivityCompat.PermissionCompatDelegate {
 
   // Simulate the permission request being granted without showing the actual permission dialog
@@ -220,4 +195,3 @@ class TestPermissionDelegate : ActivityCompat.PermissionCompatDelegate {
     return true
   }
 }
-*/
