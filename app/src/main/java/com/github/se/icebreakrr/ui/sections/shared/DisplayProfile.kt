cc9f9ef5911db9e7112b1596151247d8cbe3ecf7
Valentin Porchet <46000121+Kapestian@users.noreply.github.com>
2024-12-17 23:36:34 +0100

    fix(profiles): improve readability of profiles

--- app/src/main/java/com/github/se/icebreakrr/ui/sections/shared/DisplayProfile.kt
+++ app/src/main/java/com/github/se/icebreakrr/ui/sections/shared/DisplayProfile.kt
@@ -4,6 +4,7 @@ import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.Send
@@ -16,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -26,6 +28,7 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -48,7 +51,7 @@ import com.github.se.icebreakrr.utils.NetworkUtils.showNoInternetToast
 
 // Constants
 private val INFO_SECTION_PADDING = 16.dp
-private val INFO_SECTION_SPACING = 11.dp
+private val INFO_SECTION_SPACING = 16.dp
 private val TAG_HEIGHT_DP = 54.dp
 private val REQUEST_BUTTON_SIZE = 55.dp
 private val REQUEST_BUTTON_ELEVATION = 8.dp
@@ -57,11 +60,14 @@ private val PROFILE_IMAGE_ASPECT_RATIO = 1f
 private val PROFILE_IMAGE_PADDING = 16.dp
 private val USERNAME_TEXT_STYLE =
     TextStyle(fontSize = 26.sp, fontWeight = FontWeight.Bold, color = Color.White)
-private val INFO_TEXT_STYLE =
+private val CONTENT_TEXT_STYLE =
     TextStyle(
-        fontSize = 14.sp, lineHeight = 20.sp, fontWeight = FontWeight.W500, letterSpacing = 0.1.sp)
+        fontSize = 16.sp, lineHeight = 20.sp, fontWeight = FontWeight.W400, letterSpacing = 0.1.sp)
+private val INFO_SUBTITLE_STYLE =
+    TextStyle(
+        fontSize = 18.sp, lineHeight = 20.sp, fontWeight = FontWeight.W500, letterSpacing = 0.1.sp)
 private val CATCHPHRASE_TEXT_STYLE =
-    TextStyle(fontSize = 22.sp, lineHeight = 28.sp, fontWeight = FontWeight.W400)
+    TextStyle(fontSize = 22.sp, lineHeight = 28.sp, fontWeight = FontWeight.W400, fontStyle = FontStyle.Italic)
 
 private val DESCRIPTION_PADDING = 4.dp
 private val COLUMN_PADDING = 4.dp
@@ -95,7 +101,10 @@ fun InfoSection(profile: Profile, tagsViewModel: TagsViewModel) {
       profile.tags.map { tagString -> Pair(tagString, tagsViewModel.tagToColor(tagString)) }
 
   Column(
-      modifier = Modifier.fillMaxWidth().padding(INFO_SECTION_PADDING).testTag("infoSection"),
+      modifier = Modifier
+          .fillMaxWidth()
+          .padding(INFO_SECTION_PADDING)
+          .testTag("infoSection"),
       verticalArrangement = Arrangement.spacedBy(INFO_SECTION_SPACING, Alignment.Top),
       horizontalAlignment = Alignment.Start) {
         // Catchphrase Section
@@ -107,13 +116,8 @@ fun InfoSection(profile: Profile, tagsViewModel: TagsViewModel) {
             verticalArrangement = Arrangement.spacedBy(VERTICAL_ARRANGEMENT)) {
               Text(
                   text = "Description",
-                  style =
-                      TextStyle(
-                          fontSize = 14.sp,
-                          lineHeight = 20.sp,
-                          fontWeight = FontWeight.W500,
-                          color = MaterialTheme.colorScheme.secondary,
-                          letterSpacing = 0.1.sp))
+                  style = INFO_SUBTITLE_STYLE,
+                  color = MaterialTheme.colorScheme.secondary)
               ProfileDescription(profile.description)
             }
 
@@ -123,7 +127,7 @@ fun InfoSection(profile: Profile, tagsViewModel: TagsViewModel) {
             verticalArrangement = Arrangement.spacedBy(VERTICAL_ARRANGEMENT)) {
               Text(
                   text = "Tags",
-                  style = INFO_TEXT_STYLE,
+                  style = INFO_SUBTITLE_STYLE,
                   color = MaterialTheme.colorScheme.secondary)
               TagsSection(userTags)
             }
@@ -159,10 +163,11 @@ fun ProfileHeader(
 
   Box(
       modifier =
-          Modifier.fillMaxWidth()
-              .aspectRatio(PROFILE_IMAGE_ASPECT_RATIO)
-              .background(Color.LightGray)
-              .testTag("profileHeader")) {
+      Modifier
+          .fillMaxWidth()
+          .aspectRatio(PROFILE_IMAGE_ASPECT_RATIO)
+          .background(Color.LightGray)
+          .testTag("profileHeader")) {
 
         // Profile image
         AsyncImage(
@@ -170,9 +175,10 @@ fun ProfileHeader(
             contentDescription = "Profile Image",
             contentScale = ContentScale.Crop,
             modifier =
-                Modifier.fillMaxSize()
-                    .background(MaterialTheme.colorScheme.surfaceVariant, CircleShape)
-                    .testTag("profilePicture"),
+            Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.surfaceVariant, CircleShape)
+                .testTag("profilePicture"),
             placeholder = painterResource(id = R.drawable.nopp),
             error = painterResource(id = R.drawable.nopp))
 
@@ -180,10 +186,11 @@ fun ProfileHeader(
         IconButton(
             onClick = { navigationActions.goBack() },
             modifier =
-                Modifier.align(Alignment.TopStart)
-                    .padding(PROFILE_IMAGE_PADDING)
-                    .background(BUTTON_BG_GRADIENT, shape = CircleShape)
-                    .testTag("goBackButton")) {
+            Modifier
+                .align(Alignment.TopStart)
+                .padding(PROFILE_IMAGE_PADDING)
+                .background(BUTTON_BG_GRADIENT, shape = CircleShape)
+                .testTag("goBackButton")) {
               Icon(
                   imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                   contentDescription = "Go Back",
@@ -194,15 +201,17 @@ fun ProfileHeader(
         if (!myProfile) {
           Box(
               modifier =
-                  Modifier.align(Alignment.TopEnd)
-                      .padding(PADDING_STANDARD)
-                      .size(REQUEST_BUTTON_SIZE),
+              Modifier
+                  .align(Alignment.TopEnd)
+                  .padding(PADDING_STANDARD)
+                  .size(REQUEST_BUTTON_SIZE),
               contentAlignment = Alignment.Center) {
                 IconButton(
                     onClick = { blockReportModal = true },
                     modifier =
-                        Modifier.background(BUTTON_BG_GRADIENT, shape = CircleShape)
-                            .testTag("flagButton")) {
+                    Modifier
+                        .background(BUTTON_BG_GRADIENT, shape = CircleShape)
+                        .testTag("flagButton")) {
                       Icon(
                           painter = painterResource(id = R.drawable.flag),
                           contentDescription = "report/block user",
@@ -219,11 +228,12 @@ fun ProfileHeader(
           val buttonTag = if (myProfile) "editButton" else "requestButton"
           Box(
               modifier =
-                  Modifier.align(Alignment.BottomEnd)
-                      .padding(PADDING_STANDARD)
-                      .size(REQUEST_BUTTON_SIZE)
-                      .shadow(REQUEST_BUTTON_ELEVATION, shape = CircleShape)
-                      .background(MaterialTheme.colorScheme.primary, CircleShape),
+              Modifier
+                  .align(Alignment.BottomEnd)
+                  .padding(PADDING_STANDARD)
+                  .size(REQUEST_BUTTON_SIZE)
+                  .shadow(REQUEST_BUTTON_ELEVATION, shape = CircleShape)
+                  .background(MaterialTheme.colorScheme.primary, CircleShape),
               contentAlignment = Alignment.Center) {
                 IconButton(
                     onClick = {
@@ -248,17 +258,19 @@ fun ProfileHeader(
         // Username
         Box(
             modifier =
-                Modifier.fillMaxWidth()
-                    .align(Alignment.BottomEnd)
-                    .height(USERNAME_BOX_SIZE)
-                    .background(USERNAME_BG_GRADIENT)) {
+            Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomEnd)
+                .height(USERNAME_BOX_SIZE)
+                .background(USERNAME_BG_GRADIENT)) {
               Text(
                   text = profile.name,
                   style = USERNAME_TEXT_STYLE,
                   modifier =
-                      Modifier.align(Alignment.CenterStart)
-                          .padding(horizontal = PADDING_STANDARD)
-                          .testTag("username"))
+                  Modifier
+                      .align(Alignment.CenterStart)
+                      .padding(horizontal = PADDING_STANDARD)
+                      .testTag("username"))
             }
       }
 
@@ -275,9 +287,10 @@ fun ProfileHeader(
         }) {
           Card(
               modifier =
-                  Modifier.widthIn(max = maxWidth)
-                      .padding(PADDING_STANDARD)
-                      .testTag("alertDialogReportBlock")) {
+              Modifier
+                  .widthIn(max = maxWidth)
+                  .padding(PADDING_STANDARD)
+                  .testTag("alertDialogReportBlock")) {
                 Column(
                     modifier = Modifier.padding(PADDING_LARGE),
                     verticalArrangement = Arrangement.spacedBy(PADDING_SMALL)) {
@@ -409,7 +422,7 @@ fun ProfileHeader(
 @Composable
 fun ProfileCatchPhrase(catchPhrase: String) {
   Text(
-      text = catchPhrase,
+      text = "«${catchPhrase}»",
       style = CATCHPHRASE_TEXT_STYLE,
       color = MaterialTheme.colorScheme.onSecondary,
       textAlign = TextAlign.Left,
@@ -425,7 +438,9 @@ fun ProfileCatchPhrase(catchPhrase: String) {
  */
 @Composable
 fun TagsSection(listOfTags: List<Pair<String, Color>>) {
-  Box(modifier = Modifier.fillMaxWidth().testTag("tagSection")) {
+  Box(modifier = Modifier
+      .fillMaxWidth()
+      .testTag("tagSection")) {
     RowOfTags(listOfTags, TagStyle())
   }
 }
@@ -439,19 +454,22 @@ fun TagsSection(listOfTags: List<Pair<String, Color>>) {
 fun ProfileDescription(description: String) {
   Text(
       text = description,
-      style = INFO_TEXT_STYLE,
+      style = CONTENT_TEXT_STYLE,
       color = MaterialTheme.colorScheme.onSecondary,
-      modifier = Modifier.padding(DESCRIPTION_PADDING).testTag("profileDescription"))
+      modifier = Modifier
+          .padding(vertical = DESCRIPTION_PADDING)
+          .testTag("profileDescription"))
 }
 
 @Composable
 fun MessageWhenLoadingProfile(paddingValues: PaddingValues) {
   Box(
       modifier =
-          Modifier.fillMaxSize()
-              .background(Color.LightGray)
-              .padding(paddingValues)
-              .testTag("loadingBox"),
+      Modifier
+          .fillMaxSize()
+          .background(Color.LightGray)
+          .padding(paddingValues)
+          .testTag("loadingBox"),
       contentAlignment = Alignment.Center) {
         Text("Loading profile...", textAlign = TextAlign.Center)
       }
