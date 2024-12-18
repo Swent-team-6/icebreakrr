f8f47fc4bffa6327f1bd20052b8061d388bf5f71
MaxCastelCast <maximo.castelari@gmail.com>
2024-12-18 11:39:05 +0100

    ktfmt format

--- app/src/main/java/com/github/se/icebreakrr/model/message/MeetingRequestService.kt
+++ app/src/main/java/com/github/se/icebreakrr/model/message/MeetingRequestService.kt
@@ -128,18 +128,20 @@ class MeetingRequestService : FirebaseMessagingService() {
         val stringReason =
             when (message) {
               "DISTANCE" -> DISTANCE_REASON_CANCELLATION
-              //"TIME" -> TIME_REASON_CANCELLATION
+              // "TIME" -> TIME_REASON_CANCELLATION
               "CANCELLED" -> CANCELLED_REASON_CANCELLATION
               "CLOSED" -> CLOSED_APP_REASON_CANCELLATION
               else -> DEFAULT_REASON_CANCELLATION
             }
-        if(stringReason != DEFAULT_REASON_CANCELLATION) {
+        if (stringReason != DEFAULT_REASON_CANCELLATION) {
           MeetingRequestManager.meetingRequestViewModel?.removeFromMeetingRequestSent(senderUid) {
-            MeetingRequestManager.meetingRequestViewModel?.removeFromMeetingRequestInbox(senderUid) {
-              MeetingRequestManager.meetingRequestViewModel?.removeChosenLocalisation(senderUid) {
-                MeetingRequestManager.meetingRequestViewModel?.updateInboxOfMessages {}
-              }
-            }
+            MeetingRequestManager.meetingRequestViewModel?.removeFromMeetingRequestInbox(
+                senderUid) {
+                  MeetingRequestManager.meetingRequestViewModel?.removeChosenLocalisation(
+                      senderUid) {
+                        MeetingRequestManager.meetingRequestViewModel?.updateInboxOfMessages {}
+                      }
+                }
           }
           MeetingRequestManager.meetingRequestViewModel?.stopMeetingRequestTimer(senderUid, this)
           showNotification("Cancelled meeting with $senderName", stringReason)
